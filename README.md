cdecdcb69758dad66dad7c7ba8db2542b3a2aae3
Piyush Agrawal <poush12@gmail.com>
2017-04-16 00:57:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# WIP [ALPHA-0.0.2]
+
+
+statiko install
---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+
+var travis_pages = require('travis_pages');
+
+
+var choice = process.argv[2];
+
+if( choice == "install" )
+	travis_pages();
+
+else
+	buildAPI();
+
+function buildAPI( files ){
+
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+
+	string = {
+		updated_at: mm+'/'+dd+'/'+yyyy, 
+		data: "{{data}}"
+	};
+
+	files.forEach(function(ele){
+		fs.readFile(process.cwd() + '/' + ele, function read(err, data) {
+	    	if (err) {
+	        	return ;
+	    	}
+	    	string.data = data.toString();
+	    	fs.outputJsonSync(process.cwd() + '/api/' + ele + '.json', string);
+		});
+	})
+}
